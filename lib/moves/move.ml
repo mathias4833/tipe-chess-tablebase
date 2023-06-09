ac8162ad8156737eceae4729799b5415e55be4e4
mathias4833 <67101597+mathias4833@users.noreply.github.com>
2023-06-09 09:28:35 +0000

    small changes

--- lib/moves/move.ml
+++ lib/moves/move.ml
@@ -14,22 +14,26 @@ let rec add_moves_to_list p n bitboard acc =
 
 (* Joue le coup et renvoie la nouvelle position *)
 let play_move chessboard move =
+  let tempboard = {
+    chessboard with
+    wpawns = Bitboard.clear_nth chessboard.wpawns a;
+    wknights = Bitboard.clear_nth chessboard.wknights a;
+    wbishops = Bitboard.clear_nth chessboard.wbishops a;
+    wrooks = Bitboard.clear_nth chessboard.wrooks a;
+    wqueen = Bitboard.clear_nth chessboard.wqueen a;
+    wking = Bitboard.clear_nth chessboard.wking a;
+    bpawns = Bitboard.clear_nth chessboard.bpawns a;
+    bknights = Bitboard.clear_nth chessboard.bknights a;
+    bbishops = Bitboard.clear_nth chessboard.bbishops a;
+    brooks = Bitboard.clear_nth chessboard.brooks a;
+    bqueen = Bitboard.clear_nth chessboard.bqueen a;
+    bking = Bitboard.clear_nth chessboard.bking a;
+    iswhite: if chessboard.iswhite then false else true;
+  } in
+
   match (move, chessboard.iswhite) with
   |(Move(P, a, b), true) -> {
-      chessboard with
-      wpawns = Bitboard.clear_nth chessboard.wpawns a;
-      wknights = Bitboard.clear_nth chessboard.wknights a;
-      wbishops = Bitboard.clear_nth chessboard.wbishops a;
-      wrooks = Bitboard.clear_nth chessboard.wrooks a;
-      wqueen = Bitboard.clear_nth chessboard.wqueen a;
-      wking = Bitboard.clear_nth chessboard.wking a;
-      bpawns = Bitboard.clear_nth chessboard.bpawns a;
-      bknights = Bitboard.clear_nth chessboard.bknights a;
-      bbishops = Bitboard.clear_nth chessboard.bbishops a;
-      brooks = Bitboard.clear_nth chessboard.brooks a;
-      bqueen = Bitboard.clear_nth chessboard.bqueen a;
-      bking = Bitboard.clear_nth chessboard.bking a;
-      iswhite: if chessboard.iswhite then false else true;
+      
     }
 ;;
 
