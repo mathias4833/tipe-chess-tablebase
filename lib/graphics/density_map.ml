fc156a9167d2518b7499f91786b7f64541cf9c53
mathias4833 <67101597+mathias4833@users.noreply.github.com>
2024-05-23 12:48:49 +0200

    Fix bugs

--- lib/graphics/density_map.ml
+++ lib/graphics/density_map.ml
@@ -1,14 +1,22 @@
+open Endgame
+open Utils
 open Bimage
 open Bimage_unix
 
-let save_to_image table height width =
+let save_to_image table pieces height width =
+  let counter = ref 0 in
   let img = Image.v u8 rgb width height in
   for i = 0 to width - 1 do
     for j = 0 to height - 1 do
       let n = (j * width) + i in
-      if table.{n} <> 0 then
-        Image.set_pixel img i j (Pixel.v rgb [ 0.0; 0.0; 1.0 ])
+      let board = Serializer.number_to_board n pieces in
+      if (not (Transformations.is_normalized board pieces)) || table.{n} <> 0
+      then (
+        Image.set_pixel img i j (Pixel.v rgb [ 0.0; 0.0; 1.0 ]);
+        counter := !counter + 1)
       else Image.set_pixel img i j (Pixel.v rgb [ 1.0; 0.0; 0.0 ])
     done
   done;
+  Printf.printf "Positions gagnantes: %i, Densite: %f\n" !counter
+    (float_of_int !counter /. float_of_int (height * width));
   Stb.write_png "out.png" img
